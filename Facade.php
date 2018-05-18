09f8b4daf9ee9f7d3869ad0bc1886dd8fd5f2c01
MengYi <1203669692@qq.com>
2018-05-18 11:03:05 +0800

    外观

--- Facade.php
+++ Facade.php
@@ -1,4 +1,8 @@
 <?php
+//意图：为子系统中的一组接口提供一个一致的界面，外观模式定义了一个高层接口，这个接口使得这一子系统更加容易使用。
+
+//主要解决：降低访问复杂系统的内部子系统时的复杂度，简化客户端与之的接口。
+//在客户端和复杂系统之间再加一层，这一层将调用顺序、依赖关系等处理好
 interface Shape 
 {
   public function draw();
@@ -8,6 +12,54 @@ class Rectangle implements Shape {
 
     public function draw() {
        //System.out.println("Rectangle::draw()");
-       echo "调用三角形的draw方法"; 
+       echo "调用三角形的draw方法\n"; 
     }
  }
+
+class Square implements Shape {
+
+  public function draw() {
+     echo "调用正方形的draw方法\n";
+  }
+}
+
+class Circle implements Shape {
+
+  public function draw() {
+     echo "调用圆的draw方法\n";
+  }
+}
+
+
+class ShapeMaker {
+  public  $circle;
+  private  $rectangle;
+  private  $square;
+
+ public function __construct()
+ {
+      $this->circle = new Circle();
+      $this->rectangle  = new Rectangle();
+      $this->square = new square();
+ }
+
+  public function drawCircle(){
+
+     $this->circle->draw();
+  }
+
+  public function drawRectangle(){
+     $this->rectangle->draw();
+  }
+
+  public function drawSquare(){
+     $this->square->draw();
+  }
+}
+
+// --------------测试------------
+ $shapeMaker = new ShapeMaker();
+
+$shapeMaker->drawCircle();
+$shapeMaker->drawRectangle();
+$shapeMaker->drawSquare();
--- State.php
+++ State.php
@@ -1,5 +1,8 @@
 <?php
 
+
+//允许对象在内部状态发生改变时改变它的行为，对象看起来好像修改了它的类。
+
 interface State {
     public function doAction(context $context);
  }
